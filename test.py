93f456ee0cc6c7085fd58c278e77d5f009f47d8d
AleksaKras <49614877+AleksaKras@users.noreply.github.com>
2021-07-14 22:59:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 text_file = "passwords.txt"
-valid = 0 #count of valid passwords
-
+valid_passwords = 0  # count of valid passwords
+log = ''  # variable for logging of process
 with open(text_file, "r") as file:
-    for cur_pass in file:
-        count = 0 #count of right symbols in the password line
-        symbol = cur_pass[0] # key of the line
+    for line_of_passwords in file:
+        log += line_of_passwords  # save a line of passwords for logging of process
 
-        rule1 = int(cur_pass[2]) #first rule of pass
-        rule2 = int(cur_pass[4]) #second rule of pass
+        count_of_same_symbols = 0  # count of same symbols in the password line
+        main_symbol_of_line = line_of_passwords[0]  # main symbol of the password
 
-        password = cur_pass[7:] #password line without rules
+        rule1 = int(line_of_passwords[2])  # first rule of password
+        rule2 = int(line_of_passwords[4])  # second rule of password
 
-        for key in password:
-            if key == symbol:
-                count += 1 #if key is right, count +=1
-        if rule1 <= count <= rule2:
-            valid += 1
-print(valid)#check the valid number in the console
+        password = line_of_passwords[7:]
 
-with open(text_file, "a") as file:
-    file.write("\nValid passwords: " + str(valid)) #rewrite the file with valid number
+        for symbol in password:
+            if symbol == main_symbol_of_line:
+                count_of_same_symbols += 1  # if symbol is equal, count_of_same_symbols += 1
+
+        if rule1 <= count_of_same_symbols <= rule2:
+            valid_passwords += 1
+
+print(valid_passwords)  # check the valid passwords in the console
+
+with open("valid_passwords.txt", "a") as file:
+    file.write(str(log) + "\nValid passwords: " + str(valid_passwords) + "\n \n")  # write in a new file count of valid passwords
