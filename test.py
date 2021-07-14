846c8c96312437168d695a3c45ae8214ee7981cf
AleksaKras <49614877+AleksaKras@users.noreply.github.com>
2021-07-14 20:28:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 text_file = "passwords.txt"
-passwords = list()
-valid = 0
+valid = 0 #count of valid passwords
 
 with open(text_file, "r") as file:
     for cur_pass in file:
-        count = 0
-        symbol = cur_pass[0]
+        count = 0 #count of right symbols in the password line
+        symbol = cur_pass[0] # key of the line
 
-        rule1 = int(cur_pass[2])
-        rule2 = int(cur_pass[4])
+        rule1 = int(cur_pass[2]) #first rule of pass
+        rule2 = int(cur_pass[4]) #second rule of pass
 
-        password = cur_pass[7:]
+        password = cur_pass[7:] #password line without rules
 
         for key in password:
             if key == symbol:
-                count += 1
+                count += 1 #if key is right, count +=1
         if rule1 <= count <= rule2:
             valid += 1
-print(valid)
+print(valid)#check the valid number in the console
 
 with open(text_file, "a") as file:
-    file.write("\nValid passwords: " + str(valid))
+    file.write("\nValid passwords: " + str(valid)) #rewrite the file with valid number
