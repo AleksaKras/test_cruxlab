ebe28f71762a0806d22fd1c1626db4464a2d7a1e
AleksaKras <49614877+AleksaKras@users.noreply.github.com>
2021-07-14 23:59:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
 text_file = "passwords.txt"
-valid_passwords = 0  # count of valid passwords
+count_of_valid_passwords = 0
 log = ''  # variable for logging of process
 with open(text_file, "r") as file:
     for line_of_passwords in file:
         log += line_of_passwords  # save a line of passwords for logging of process
 
-        count_of_same_symbols = 0  # count of same symbols in the password line
-        main_symbol_of_line = line_of_passwords[0]  # main symbol of the password
+        required_symbol_of_line = line_of_passwords[0]  # required symbol of the password
 
-        rule1 = int(line_of_passwords[2])  # first rule of password
-        rule2 = int(line_of_passwords[4])  # second rule of password
+        min_quantity_of_repetition = int(line_of_passwords[2])  # first rule of password
+        max_quantity_of_repetition = int(line_of_passwords[4])  # second rule of password
 
         password = line_of_passwords[7:]
 
-        for symbol in password:
-            if symbol == main_symbol_of_line:
-                count_of_same_symbols += 1  # if symbol is equal, count_of_same_symbols += 1
+        count_of_same_symbols = password.count(required_symbol_of_line)
 
-        if rule1 <= count_of_same_symbols <= rule2:
-            valid_passwords += 1
+        if min_quantity_of_repetition <= count_of_same_symbols <= max_quantity_of_repetition:
+            count_of_valid_passwords += 1
 
-print(valid_passwords)  # check the valid passwords in the console
-
+print(count_of_valid_passwords)  # check the valid passwords in the console
+log += "\nValid passwords: " + str(count_of_valid_passwords) + "\n \n"
 with open("valid_passwords.txt", "a") as file:
-    file.write(str(log) + "\nValid passwords: " + str(valid_passwords) + "\n \n")  # write in a new file count of valid passwords
+    file.write(log)
